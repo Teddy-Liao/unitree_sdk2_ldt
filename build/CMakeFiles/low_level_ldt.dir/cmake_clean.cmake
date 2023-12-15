d227f611723cf5e5f0c7abf44a79e16d23e07756
Liao Dengting <540614105@qq.com>
2023-12-15 13:44:12 +0800

    Initial commit

---
+++ build/CMakeFiles/low_level_ldt.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/low_level_ldt.dir/example/user/low_level_ldt.cpp.o"
+  "low_level_ldt"
+  "low_level_ldt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/low_level_ldt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
